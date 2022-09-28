fcf42312408e4dacbf0614d371212db2d1797ea2
samhayder <sams.seul@gmail.com>
2022-09-28 23:14:06 +0600

    String Builder

--- Strings.java
+++ Strings.java
@@ -45,7 +45,7 @@ public class Strings {
         System.out.println(arrTotalLength);*/
 
         //02. Input a string from the user. Create a new string called ‘result’ in which you will replace the letter ‘e’ in the original string with letter ‘i’.
-        String repStr  = sc.nextLine();
+        /*String repStr  = sc.nextLine();
         String result = "";
         for (int i = 0; i < repStr.length(); i++){
 
@@ -55,7 +55,44 @@ public class Strings {
                 result += repStr.charAt(i);
             }
         }
-        System.out.println(result);
+        System.out.println(result);*/
+
+        //03. Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
+       /* String email = sc.nextLine();
+        String userName = "";
+        for (int i = 0; i < email.length(); i++){
+            if (email.charAt(i) == '@'){
+                break;
+            } else {
+                userName += email.charAt(i);
+            }
+        }
+        System.out.println(userName);*/
+
+        //********* String Builder *******************
+        StringBuilder sb = new StringBuilder("Congratulation");
+        //Set Char at index
+        sb.setCharAt(0,'O');
+        //Inset
+        sb.insert(0,'C');
+        //Delete
+        sb.delete(1,2);
+        //Append
+        sb.append('!');
+        System.out.println(sb);
+
+        //Reverse
+        for (int i = 0; i < sb.length() / 2; i++){
+            int front = i;
+            int back = sb.length() - i - 1;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println(sb);
 
 
 
